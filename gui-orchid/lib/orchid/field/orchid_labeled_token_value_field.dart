a294d7797985fe0b65ade8f409881ab42c0f595b
Patrick Niemeyer <pat@pat.net>
2023-07-28 17:58:56 -0500

    WIP

--- gui-orchid/lib/orchid/field/orchid_labeled_token_value_field.dart
+++ gui-orchid/lib/orchid/field/orchid_labeled_token_value_field.dart
@@ -1,4 +1,3 @@
-// @dart=2.9
 import 'package:orchid/common/rounded_rect.dart';
 import 'package:orchid/orchid/orchid.dart';
 import 'package:orchid/api/orchid_eth/token_type.dart';
@@ -14,21 +13,21 @@ import 'token_value_widget_row.dart';
 class OrchidLabeledTokenValueField extends StatelessWidget {
   final TypedTokenValueFieldController controller;
   final TokenType type;
-  final USD usdPrice;
-  final String label;
-  final double labelWidth;
+  final USD? usdPrice;
+  final String? label;
+  final double? labelWidth;
   final bool enabled;
-  final bool readOnly;
-  final VoidCallback onClear;
-  final String hintText;
-  final Widget trailing;
-  final Widget bottomBanner;
+  final bool? readOnly;
+  final VoidCallback? onClear;
+  final String? hintText;
+  final Widget? trailing;
+  final Widget? bottomBanner;
 
   // TODO: enhance this to include a message
   final bool error;
 
   OrchidLabeledTokenValueField({
-    Key key,
+    Key? key,
     required this.type,
     required this.controller,
     this.enabled = true,
@@ -108,7 +107,7 @@ class OrchidLabeledTokenValueField extends StatelessWidget {
 }
 
 class TypedTokenValueFieldController extends ValueFieldController<Token> {
-  final TokenType type;
+  final TokenType? type;
 
   TypedTokenValueFieldController({
     required this.type,
@@ -116,27 +115,27 @@ class TypedTokenValueFieldController extends ValueFieldController<Token> {
 
   /// Return the value, zero if empty, or null if invalid
   @override
-  Token get value {
+  Token? get value {
     if (type == null) {
       return null;
     }
     final text = textController.text;
-    if (text == null || text == '') {
-      return type.zero;
+    if (text.isEmpty || text == '') {
+      return type!.zero;
     }
     try {
       var value = double.parse(
         // Allow comma as decimal separator for localization
         text.replaceAll(',', '.'),
       );
-      return type.fromDouble(value);
+      return type!.fromDouble(value);
     } catch (err) {
       return null;
     }
   }
 
   @override
-  set value(Token value) {
-    textController.text = value.floatValue.toString();
+  set value(Token? value) {
+    textController.text = value == null ? '' : value.floatValue.toString();
   }
 }
