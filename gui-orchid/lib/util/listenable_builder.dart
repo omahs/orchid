ab25740dfee4d6563d3955f0853cafd89b79b984
Patrick Niemeyer <pat@pat.net>
2023-08-06 14:00:29 -0500

    WIP

--- gui-orchid/lib/util/listenable_builder.dart
+++ gui-orchid/lib/util/listenable_builder.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+// TODO: This is no longer needed in later versions of Flutter. Remove when redundant.
+
 /// This is a trivial subclass of AnimatedBuilder that serves to rename
 /// it more appropriately for use as a plain listenable builder.
 /// (This really should be the name of the base class in Flutter core.)
